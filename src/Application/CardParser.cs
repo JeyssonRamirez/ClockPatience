54ccc1cf33711930a44f6cf99a26a16b4817bb58
JSON_LAPTOP\jeyss <jeysson.ramirez@outlook.com>
2025-11-04 23:01:31 +0100

    add parser

---
+++ src/Application/CardParser.cs
@@ -0,0 +1,33 @@
+﻿using Application.Definition;
+using Core.Entities;
+using Crosscutting.Util.Extension;
+
+namespace Application
+{
+    public class CardParser : ICardParser
+    {
+        public Card ParseCard(string cardString)
+        {
+            if (string.IsNullOrWhiteSpace(cardString) || cardString.Length != 2)
+                throw new ArgumentException($"Invalid card format: {cardString}");
+
+            return new Card(CustomEnumExtensions.GetFromStringValue<CardType>(cardString[0].ToString()), cardString[1]);
+        }
+
+        public List<Card> ParseDeck(List<string> lines)
+        {
+            var cards = new List<Card>();
+
+            foreach (var line in lines)
+            {
+                var cardStrings = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                foreach (var cardString in cardStrings)
+                {
+                    cards.Add(ParseCard(cardString));
+                }
+            }
+
+            return cards;
+        }
+    }
+}
--- src/Core.Entities/CardType.cs
+++ src/Core.Entities/CardType.cs
@@ -1,19 +1,34 @@
-﻿namespace Core.Entities
+﻿using Crosscutting.Util.Extension;
+
+namespace Core.Entities
 {
     public enum CardType
     {
-       Ace = 1,
-                Two = 2,
-                Tree = 3,
-                Four = 4,
-                Five = 5,
-                Six = 6,
-                Seven = 7,
-                Eig = 8,
-                Nine = 9,
-                Ten = 10,
-                Jack = 11,
-                Queen  = 12,
-                King = 13,
+        [StringValue("A")]
+        Ace = 1,
+        [StringValue("2")]
+        Two = 2,
+        [StringValue("3")]
+        Tree = 3,
+        [StringValue("4")]
+        Four = 4,
+        [StringValue("5")]
+        Five = 5,
+        [StringValue("6")]
+        Six = 6, 
+        [StringValue("7")]
+        Seven = 7, 
+        [StringValue("8")]
+        Eig = 8, 
+        [StringValue("9")]
+        Nine = 9,
+        [StringValue("10")]
+        Ten = 10, 
+        [StringValue("J")]
+        Jack = 11, 
+        [StringValue("Q")]
+        Queen = 12,
+        [StringValue("K")]
+        King = 13,
     }
 }
